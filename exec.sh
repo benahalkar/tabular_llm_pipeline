cdc4c5cfbe14bc954fc5862e3b0f083015d68dd7
Harsh Benahalkar <benahalkar@gmail.com>
2025-01-09 21:29:01 -0500

    updating the bash script

--- exec.sh
+++ exec.sh
@@ -3,32 +3,40 @@ FILE_PATH=`pwd`
 
 export WANDB_MODE=disabled
 
+    # --deepspeed ./zero2.json \
+
 # CUDA_LAUNCH_BLOCKING=1 deepspeed ${FILE_PATH}/train.py \
 CUDA_LAUNCH_BLOCKING=1 ${PYTHON_PATH} ${FILE_PATH}/train.py \
-    --deepspeed ./zero2.json \
     --model_name_or_path ai21labs/Jamba-tiny-random \
     --data_path ./data \
+    --device cuda \
+    --optim adamw_torch \
     --bits 8 \
     --lora_enable True \
-    --fp16 False \
+    --lora_r 64 \
+    --lora_threshold 6.0 \
+    --fp16 True \
     --bf16 False \
-    --threshold 6.0 \
-    --num_train_epochs 10 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
+    --tf32 False \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
     --gradient_accumulation_steps 1 \
-    --eval_strategy "no" \
+    --evaluation_strategy "steps" \
+    --eval_steps 100 \
     --save_strategy "steps" \
-    --save_steps 50000 \
+    --save_steps 100 \
     --save_total_limit 1 \
     --learning_rate 2e-5 \
-    --weight_decay 0. \
+    --weight_decay 0.1 \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
-    --logging_steps 1 \
-    --tf32 False \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --output_dir ./finetuned_model \
-    --device cuda
+    --logging_dir ./logs \
+    --logging_steps 10 \
+    --learning_rate 2e-3 \
+    --load_best_model_at_end True \
+    --report_to wandb
 
