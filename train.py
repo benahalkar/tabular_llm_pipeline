2d7f444150ee9a634c9a985a17645042ad4e1639
Harsh Benahalkar <benahalkar@gmail.com>
2025-01-09 21:29:27 -0500

    adding updated train

--- train.py
+++ train.py
@@ -1,64 +1,90 @@
 import os
 import json
-import pandas as pd
+import pytz
 import torch
-import numpy as np
-import time
 import warnings
+import numpy as np
+import pandas as pd
 from copy import deepcopy
+from datetime import datetime
+from dataclasses import dataclass
 from torch.utils.data import Dataset
-from typing import Dict, Optional
 from dataclasses import dataclass, field
-import multiprocessing
-from transformers import HfArgumentParser, AutoTokenizer, TrainingArguments, PreTrainedTokenizer, AutoConfig, AutoModelForCausalLM, Trainer
+from typing import Dict, Optional, Sequence, List
+from transformers import set_seed as transformers_set_seed
+from pynvml import nvmlInit, nvmlDeviceGetHandleByIndex, nvmlDeviceGetMemoryInfo, nvmlShutdown
+from transformers import AutoConfig, AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, TrainingArguments, Trainer, HfArgumentParser, TrainerCallback, PreTrainedTokenizer
 
-from constants import IGNORE_INDEX, TABLE_TOKEN_INDEX, DEFAULT_TABLE_TOKEN, DEFAULT_TABLE_START_TOKEN, DEFAULT_TABLE_END_TOKEN, TABLE_PLACEHOLDER, COL_SEPARATOR, ROW_SEPARATOR
 import conversation as conversation_lib
-
-torch.manual_seed(42)
-torch.set_printoptions(threshold=float('inf'))
-multiprocessing.set_start_method('spawn', force=True)
-
-@dataclass
-class ModelArguments:
-    model_name_or_path: str = field(default=None, metadata={"help": "Path to the model."})
-    attn_implementation: str = field(default=None)
-    freeze_backbone: bool = field(default=False)
+from constants import IGNORE_INDEX, TABLE_TOKEN_INDEX, DEFAULT_TABLE_TOKEN, DEFAULT_TABLE_START_TOKEN, DEFAULT_TABLE_END_TOKEN, TABLE_PLACEHOLDER, COL_SEPARATOR, ROW_SEPARATOR
 
 
 @dataclass
-class DataArguments:
+class CustomModelArguments:
+    model_name_or_path: Optional[str] = field(default="facebook/opt-125m")
+    attn_implementation: str = None
+
+@dataclass
+class CustomDataArguments:
     data_path: str = field(default=None, metadata={"help": "Path to the training data."})
 
-
 @dataclass
-class TrainingArguments(TrainingArguments):
+class CustomTrainingArguments(TrainingArguments):
+    device: str = "cpu"
     cache_dir: Optional[str] = field(default=None)
     optim: str = field(default="adamw_torch")
-    device: str = field(default="cpu")
-    bits: int = field(default=16)
-    threshold: float = field(default=6.0)
-    lora_enable: bool = field(default=False)
-    fp32_cpu_offload: bool = field(default=False)
-    has_fp16_weight: bool = field(default=False)
-    quant_type: str = field(default="nf4")
-    double_quant: bool = field(default=False)
-    quant_storage: Optional[torch.dtype] = field(default=torch.uint8)
-    lora_enable: bool = field(default=False)
-    lora_r: int = field(default=64)
-    lora_alpha: int = field(default=16)
-    lora_dropout: float = field(default=0.05)
-    lora_weight_path: str = field(default="")
-    lora_bias: str = field(default="none")
-    model_max_length: int = field(default=512)
-    remove_unused_columns: bool = field(default=False)
-    distributed_state: str = field(default=None)
+    model_max_length: int = field(
+        default=512,
+        metadata={
+            "help":
+            "Maximum sequence length. Sequences will be right padded (and possibly truncated)."
+        },
+    )
+    double_quant: bool = field(
+        default=True,
+        metadata={"help": "Compress the quantization statistics through double quantization."}
+    )
+    quant_type: str = field(
+        default="nf4",
+        metadata={"help": "Quantization data type to use. Should be one of `fp4` or `nf4`."}
+    )
+    bits: int = field(
+        default=16,
+        metadata={"help": "How many bits to use."}
+    )
+    lora_enable: bool = False
+    lora_r: int = 64
+    lora_threshold: float = 6.0
+    lora_alpha: int = 16
+    lora_dropout: float = 0.05
+    lora_weight_path: str = ""
+    lora_bias: str = "none"
+    report_to: str = None
 
 
-local_rank = None
-def rank0_print(*args):
-    if local_rank == 0:
-        print(*args)
+
+def get_filename():
+    est_timezone = pytz.timezone('US/Eastern')
+    current_est_time = datetime.now(est_timezone).strftime("%Y%m%d_%H%M%S")
+    return f"./training_logs/logs_{current_est_time}.txt"
+
+FILENAME = get_filename()
+
+
+def set_seed(seed=42):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    transformers_set_seed(seed)
+    torch.set_printoptions(threshold=float('inf'))
+
+    print(f"Seed set to: {seed}")
+
+
+
+def print_name_and_size(model):
+    for name, param in model.named_parameters():
+        print(f"Layer: {name} | Gradient: {param.requires_grad}")
+
 
 
 def find_all_linear_names(model):
@@ -79,6 +105,39 @@ def find_all_linear_names(model):
 
     return list(lora_module_names)
 
+
+
+def print_gpu_memory(message):
+    if torch.cuda.is_available():
+        with open(FILENAME, "a") as f:
+            f.write("\n" + message + "\n")
+            f.write(f"GPU memory allocated: {torch.cuda.memory_allocated() / 1024**2:.2f} MB\n")
+            f.write(f"GPU memory reserved: {torch.cuda.memory_reserved() / 1024**2:.2f} MB\n")
+            f.close()
+
+
+def just_logging(message):
+    with open(FILENAME, "a") as f:
+        print(">>")
+        f.write(message + "\n")
+        f.close()
+
+
+class MemoryMonitorCallback(TrainerCallback):
+    def __init__(self, print_interval=1):
+        self.print_interval = print_interval
+        nvmlInit()
+        self.handle = nvmlDeviceGetHandleByIndex(0)
+
+    def on_step_end(self, args, state, control, **kwargs):
+        if state.global_step % self.print_interval == 0:
+            info = nvmlDeviceGetMemoryInfo(self.handle)
+            just_logging(f"Step {state.global_step}: GPU memory occupied: {info.used//1024**2} MB")
+
+    def on_train_end(self, args, state, control, **kwargs):
+        nvmlShutdown()
+
+    
 
 
 def preprocess_prompts(
@@ -114,13 +173,7 @@ def preprocess_jamba(
         matches = np.all(window_view == sub_array, axis=1)
         return np.where(matches)[0]
 
-    # print(conversations)
-    
-    conv = tokenizer.tokenize(conversations)
-    # print(conv)
-    conv = tokenizer.encode(conv)
-    # print(conv)
-    
+    conv = tokenizer(conversations).input_ids
 
     assistant_command = "\nAssistant:"
     human_command = "\nHuman:"
@@ -134,10 +187,6 @@ def preprocess_jamba(
     assert len(human_indices) == len(assistant_indices), "Conversation is not identical"
     assert len(human_indices) > 0, "No indices found"
 
-    # print(">> assistant", tokenizer.tokenize(assistant_command), assistant_token, tokenizer.tokenize(human_command), "human", human_token)
-    # print(">> assistant indices", assistant_indices, "human indices", human_indices)
-    # print("total length", len(conv))
-
     input_ids = torch.tensor(conv) 
     labels = input_ids.clone()
 
@@ -149,13 +198,11 @@ def preprocess_jamba(
     for i, j in zip(human_indices, assistant_indices):
         labels[i:j] = IGNORE_INDEX
 
-    # print("input_ids\n", input_ids)
-    # print("labels\n", labels)
-    
     return dict(
         input_ids=input_ids,
         labels=labels
     )   
+
 
 @dataclass
 class SupervisedDataset(Dataset):
@@ -182,13 +229,23 @@ class SupervisedDataset(Dataset):
     
 
     def __getitem__(self, idx):
+
         sample = self.dataset[idx]
         table_filename = sample["table"]
         table_filepath = os.path.join(self.table_foldername, table_filename)
 
         df = pd.read_csv(table_filepath)
         TABLE = ROW_SEPARATOR.join(df.apply(lambda row: COL_SEPARATOR.join(row.astype(str)), axis=1))
-        
+                
+        result = []
+        for index, row in df.iterrows():
+            row_str = f"Row {index + 1}: " + ", ".join([f"Column {col + 1} value: {row[col]}" for i, col in enumerate(df.columns)])
+            result.append(row_str)
+
+        # Join all rows into a single string
+        final_string = ". ".join(result) + "."
+        print(final_string)
+
         conversations = preprocess_prompts(
             conversations=deepcopy(sample["conversations"]),
         )
@@ -203,17 +260,20 @@ class SupervisedDataset(Dataset):
 
         return data_dict
 
+
 @dataclass
 class DataCollatorForSupervisedDataset(object):
 
     tokenizer: PreTrainedTokenizer
-    training_args: TrainingArguments
 
     def __call__(self, instances):
 
+
         input_ids, labels = tuple([instance[key] for instance in instances]
                                     for key in ("input_ids", "labels"))
-
+        
+        print_gpu_memory(f"Before loading batched data of size: {len(input_ids)}")
+        
         input_ids = torch.nn.utils.rnn.pad_sequence(
             input_ids,
             batch_first=True,
@@ -235,29 +295,28 @@ class DataCollatorForSupervisedDataset(object):
         labels = labels[:, :self.tokenizer.model_max_length]
         attention_mask = input_ids.ne(self.tokenizer.pad_token_id)
 
-        print("input_ids\n", input_ids, "labels\n", labels, "attention_mask\n", attention_mask)
 
         batch = dict(
-            input_ids=input_ids.to(self.training_args.device),
-            labels=labels.to(self.training_args.device),
-            attention_mask=attention_mask.to(self.training_args.device),
+            input_ids=input_ids,
+            labels=labels,
+            attention_mask=attention_mask,
         )
 
+        print_gpu_memory(f"After loading batched data of size: {len(input_ids)}")
+
         return batch
+
+
     
-     
 def making_data_management_module(
     tokenizer,
     data_args,
-    training_args
 ):
     train_dataset = SupervisedDataset(
         tokenizer=tokenizer,
         data_args=data_args,
         train=True
     )
-
-    # print(">>", train_dataset[0])
 
     eval_dataset = SupervisedDataset(
         tokenizer=tokenizer,
@@ -267,7 +326,6 @@ def making_data_management_module(
     
     data_collator = DataCollatorForSupervisedDataset(
         tokenizer=tokenizer,
-        training_args=training_args
     )
 
     return dict(
@@ -277,78 +335,74 @@ def making_data_management_module(
     )
 
 
-
-
-
 def train():
-    global local_rank
+    parser = HfArgumentParser((CustomModelArguments, CustomDataArguments, CustomTrainingArguments))
 
-    parser = HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
-    local_rank = training_args.local_rank
     compute_dtype = (torch.float16 if training_args.fp16 else (torch.bfloat16 if training_args.bf16 else torch.float32))
+    print_gpu_memory("Before loading model")
 
-    bnb_model_from_pretrained_args = {}
+    bnb_model_from_pretrained_args = {} 
     if training_args.bits in [4, 8]:
-
-        """
-        https://huggingface.co/docs/transformers/v4.46.3/en/main_classes/quantization#transformers.BitsAndBytesConfig
-        """
         from transformers import BitsAndBytesConfig
-
+        
+        # Set up quantization configuration
         bnb_model_from_pretrained_args.update(dict(
             device_map={"": training_args.device},
             quantization_config = BitsAndBytesConfig(
-                load_in_8bit=training_args.bits == 8,
                 load_in_4bit=training_args.bits == 4,
-                llm_int8_threshold=training_args.threshold,
-                llm_int8_skip_modules=["lm_head"],
-                llm_int8_enable_fp32_cpu_offload=training_args.fp32_cpu_offload,
-                llm_int8_has_fp16_weight=training_args.has_fp16_weight,
+                llm_int8_threshold=training_args.lora_threshold,
+                llm_int8_has_fp16_weight=False,
                 bnb_4bit_compute_dtype=compute_dtype,
-                bnb_4bit_quant_type=training_args.quant_type,
                 bnb_4bit_use_double_quant=training_args.double_quant,
-                bnb_4bit_quant_storage=training_args.quant_storage,
+                bnb_4bit_quant_type=training_args.quant_type,
+                llm_int4_skip_modules=["mamba"]
             )
         ))
 
+        print("Bits and Bytes config created")
 
-    if "jamba" in model_args.model_name_or_path.lower(): 
-        config = AutoConfig.from_pretrained(model_args.model_name_or_path)
+
+    # Load tokenizer and model
+    if "jamba" in model_args.model_name_or_path.lower():
+
+        config = AutoConfig.from_pretrained(
+            model_args.model_name_or_path
+        )
 
         model = AutoModelForCausalLM.from_pretrained(
             model_args.model_name_or_path,
             trust_remote_code=True,
-            cache_dir=training_args.cache_dir,
+            # config=config,
+            use_cache=False,
             attn_implementation=model_args.attn_implementation,
-            config=config,
-            torch_dtype=compute_dtype,
+            # use_mamba_kernels=False  # Disable mamba kernels if you encounter errors
             **bnb_model_from_pretrained_args
+        )
+
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_args.model_name_or_path,
+            legacy=False,
+            cache_dir=None,
+            padding_side="right",
+            use_fast=False,
+            trust_remote_code=True,
         )
 
     else:
         raise NotImplementedError(f"Unknown model type '{model_args.model_name_or_path}' found")
     
 
-    model.config.use_cache = False
-
-
-    if model_args.freeze_backbone:
-        model.model.requires_grad_(False)
+    extra_tokens = [DEFAULT_TABLE_TOKEN, DEFAULT_TABLE_START_TOKEN, DEFAULT_TABLE_END_TOKEN, TABLE_PLACEHOLDER, COL_SEPARATOR, ROW_SEPARATOR]
+    tokenizer.add_special_tokens({'additional_special_tokens': extra_tokens})
+    model.resize_token_embeddings(len(tokenizer))
+    
 
     if training_args.bits in [4, 8]:
         from peft import prepare_model_for_kbit_training
-        model.config.torch_dtype=compute_dtype
+        model.config.torch_dtype=(torch.float16 if training_args.fp16 else (torch.bfloat16 if training_args.bf16 else torch.float32))
         model = prepare_model_for_kbit_training(model, use_gradient_checkpointing=training_args.gradient_checkpointing)
-
-    if training_args.gradient_checkpointing:
-        if hasattr(model, "enable_input_require_grads"):
-            model.enable_input_require_grads()
-        else:
-            def make_inputs_require_grad(module, input, output):
-                output.requires_grad_(True)
-            model.get_input_embeddings().register_forward_hook(make_inputs_require_grad)
 
 
     if training_args.lora_enable:
@@ -356,70 +410,63 @@ def train():
         lora_config = LoraConfig(
             r=training_args.lora_r,
             lora_alpha=training_args.lora_alpha,
-            target_modules=find_all_linear_names(model),
+            target_modules=["embed_tokens", "x_proj", "in_proj", "out_proj"],
             lora_dropout=training_args.lora_dropout,
             bias=training_args.lora_bias,
-            task_type="CAUSAL_LM",
+            task_type="CAUSAL_LM"
         )
+
         if training_args.bits == 16:
             if training_args.bf16:
                 model.to(torch.bfloat16)
             if training_args.fp16:
                 model.to(torch.float16)
-        rank0_print("Adding LoRA adapters...")
+
         model = get_peft_model(model, lora_config)
 
 
-
-    if "jamba" in model_args.model_name_or_path.lower(): 
-        tokenizer = AutoTokenizer.from_pretrained(
-            model_args.model_name_or_path,
-            legacy=False,
-            cache_dir=None,
-            # model_max_length=512,
-            padding_side="right",
-            use_fast=False,
-            trust_remote_code=True,
-        )
-    else:
-        raise NotImplementedError(f"Unknown tokenizer type '{model_args.model_name_or_path}' found")
-
-    extra_tokens = [DEFAULT_TABLE_TOKEN, DEFAULT_TABLE_START_TOKEN, DEFAULT_TABLE_END_TOKEN, TABLE_PLACEHOLDER, COL_SEPARATOR, ROW_SEPARATOR]
-    tokenizer.add_special_tokens({'additional_special_tokens': extra_tokens})
-    model.resize_token_embeddings(len(tokenizer))
-    for token in extra_tokens:
-        tok = tokenizer.convert_tokens_to_ids(token)
-        print(f"token value of {token} (ID: {tok})")
-    print(" ")
-
-
-    if training_args.bits in [4, 8]:
-        from peft.tuners.lora import LoraLayer
-        for name, module in model.named_modules():
-            if isinstance(module, LoraLayer):
-                if training_args.bf16:
-                    module = module.to(torch.bfloat16)
-            if 'norm' in name:
-                module = module.to(torch.float32)
-            if 'lm_head' in name or 'embed_tokens' in name:
-                if hasattr(module, 'weight'):
-                    if training_args.bf16 and module.weight.dtype == torch.float32:
-                        module = module.to(torch.bfloat16)
-
+    training_arguments = TrainingArguments(
+        output_dir=training_args.output_dir,
+        num_train_epochs=training_args.num_train_epochs,
+        per_device_train_batch_size=training_args.per_device_train_batch_size,
+        per_device_eval_batch_size=training_args.per_device_eval_batch_size,
+        gradient_accumulation_steps=training_args.gradient_accumulation_steps,
+        logging_dir=training_args.logging_dir,
+        logging_steps=training_args.logging_steps,
+        learning_rate=training_args.learning_rate,
+        save_strategy=training_args.save_strategy,
+        save_steps=training_args.save_steps,
+        evaluation_strategy=training_args.evaluation_strategy,
+        eval_steps=training_args.eval_steps,
+        load_best_model_at_end=training_args.load_best_model_at_end,
+        report_to=training_args.report_to,
+        gradient_checkpointing=True,
+        gradient_checkpointing_kwargs={'use_reentrant': False},
+    )
 
     data_module = making_data_management_module(
         tokenizer=tokenizer,
         data_args=data_args,
-        training_args=training_args
     )
 
     trainer = Trainer(
         model=model,
-        args=training_args,
-        **data_module
-    ) 
+        args=training_arguments,
+        **data_module,
+    )
+    trainer.add_callback(MemoryMonitorCallback)
     
-    trainer.train()    
-    
+    print_gpu_memory("After loading model")
+
+
+    total_params = sum(p.numel() for p in model.parameters())
+    print(total_params, total_params * 4 / (1024 * 1024))
+    dtypes = [p.dtype for p in model.parameters()]
+    print(dtypes)
+    print(set(dtypes))
+
+    trainer.train()
+
+
 if __name__ == "__main__":
     train()
